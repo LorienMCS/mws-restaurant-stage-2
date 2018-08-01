d8063fb11de6d70729a9709d50e407bb232959ee
Lorien <LorienMCS@gmail.com>
2018-08-01 16:12:30 -0700

    feat: Add necessary stage 1 files to stage 2

---
+++ assets/sw.js
@@ -0,0 +1,28 @@
+let cacheName = 'restaurants-cache-v2';
+let urlsToCache = [
+  '/',
+  '/restaurant.html',
+  '/css/styles.css',
+  '/css/responsive.css',
+  '/js/dbhelper.js',
+  '/js/main.js',
+  '/js/restaurant_info.js',
+  '/img/',
+  '/data/restaurants.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      cache.addAll(urlsToCache);
+    })
+  )
+})
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  )
+})
